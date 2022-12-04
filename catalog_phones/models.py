fba1a20d9ccb1f893ad510e924a896b8e5913e66
Виталий Чухляев <vitaliy69@mail.ru>
2022-12-04 13:11:57 +0300

    I create model 'Phone'

--- catalog_phones/models.py
+++ catalog_phones/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Phone(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+    image = models.URLField(max_length=300)
+    release_date =  models.DateField()
+    lte_exists = models.BooleanField(null=True)
+    slug = models.SlugField()
